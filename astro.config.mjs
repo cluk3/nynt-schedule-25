02fb96ab6391ee7e41ba4e0e17c22d837a2782d2
luca.campli <campli.luca@gmail.com>
2024-12-24 15:46:32 +0100

    add PWA support with service worker integration and Toast notifications for updates add PostHog analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,44 @@
 import { defineConfig } from "astro/config";
+import AstroPWA from "@vite-pwa/astro";
 
 export default defineConfig({
   site: "https://nynt-schedule.netlify.app/",
+  integrations: [
+    AstroPWA({
+      /* other options */
+      /* enable sw on development */
+      base: "/",
+      scope: "/",
+      includeAssets: ["favicon.svg"],
+      registerType: "autoUpdate",
+      manifest: {
+        name: "NYNT 25 Schedule",
+        short_name: "NYNT25",
+        description: "The schedule for NYNT 25",
+        theme_color: "#ab0f29",
+        icons: [
+          {
+            src: "favicon.svg",
+            sizes: "192x192",
+            type: "image/svg+xml",
+          },
+          {
+            src: "favicon.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: "/",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
+
+      devOptions: {
+        enabled: true,
+        /* other options */
+      },
+      navigateFallback: "/404",
+    }),
+  ],
 });
